d309323b70ce621533f00df4def79f491d95eac1
andresha20 <andreshenao.2021@gmail.com>
2023-03-22 16:58:43 -0500

    22/03/2023 - 14:59

--- Generator.java
+++ Generator.java
@@ -14,9 +14,12 @@ public class Generator {
             ArrayList<Object> medicamentos = new ArrayList<Object>();
             ArrayList<Object> clientes = new ArrayList<Object>();
             try (Scanner parser = new Scanner(System.in)) {
-                String outputChatbox = "¿Deseas ingresar información o leerla? 1 = Leer | 2 = Ingresar";
+                String outputChatbox = "¿Qué acción desea realizar? 1 = Leer | 2 = Ingresar | 3 = Editar | 4 = Eliminar";
                 System.out.println(outputChatbox);
                 prototype = parser.nextInt();
+                String nombre;
+                String data;
+                int indexOftargetElement;
                 switch (prototype) {
                     case 1:
                         outputChatbox = "¿Qué tipo de información deseas leer? 1 = Mascota | 2 = Medicamento | 3 = Cliente";
@@ -24,14 +27,13 @@ public class Generator {
                         prototype = parser.nextInt();
                         switch (prototype) {
                             case 1:
-                                System.out.println(mascotas.size());
                                 if (mascotas.size() == 0) {
                                     outputChatbox = "No se han agregado mascotas a la lista.";
                                     System.out.println(outputChatbox);
                                     generator.reset();
                                 }
                                 for (int i = 0; i < mascotas.size(); i++) {
-                                    System.out.println(mascotas.get(i));
+                                    System.out.println(i + ". " + mascotas.get(i));
                                 }
                                 break;
                             case 2:
@@ -41,7 +43,7 @@ public class Generator {
                                     generator.reset();
                                 }
                                 for (int i = 0; i < medicamentos.size(); i++) {
-                                    System.out.println(medicamentos.get(i));
+                                    System.out.println(i + ". " + medicamentos.get(i));
                                 }
                                 break;
                             case 3:
@@ -51,7 +53,7 @@ public class Generator {
                                     generator.reset();
                                 }
                                 for (int i = 0; i < clientes.size(); i++) {
-                                    System.out.println(clientes.get(i));
+                                    System.out.println(i + ". " + clientes.get(i));
                                 }
                                 break;
                             default:
@@ -61,8 +63,6 @@ public class Generator {
                         break;
    
                     case 2:
-                        String nombre;
-                        String data;
                         outputChatbox = "¿Qué tipo de dato deseas ingresar? 1 = Mascota | 2 = Medicamento | 3 = Cliente";
                         System.out.println(outputChatbox);
                         prototype = parser.nextInt();
@@ -116,7 +116,168 @@ public class Generator {
                                 break;
                         }
                         break;
-                
+                    case 3:
+                        outputChatbox = "¿Qué tipo de información deseas editar? 1 = Mascota | 2 = Cliente | 3 = Medicamento";
+                        System.out.println(outputChatbox);
+                        prototype = parser.nextInt();
+                        switch (prototype) {
+                            case 1:
+                                if (mascotas.size() == 0) {
+                                    outputChatbox = "No se han agregado mascotas a la lista.";
+                                    System.out.println(outputChatbox);
+                                    generator.reset();
+                                }
+                                for (int i = 0; i < mascotas.size(); i++) {
+                                    System.out.println("(" + i + ")" + mascotas.get(i));
+                                }
+                                outputChatbox = "Selecciona el indice del elemento que deseas editar:";
+                                System.out.println(outputChatbox);
+                                indexOftargetElement = parser.nextInt();
+                                outputChatbox = "La mascota seleccionada a la cual se le cambiará la información es:";
+                                System.out.println(outputChatbox);
+                                System.out.println(mascotas.get(indexOftargetElement));
+                                outputChatbox = "Ingresa el nombre de la mascota:";
+                                System.out.println(outputChatbox);
+                                nombre = parser.next();
+                                outputChatbox = "Ingresa la edad, peso, cédula del cliente, raza y medicamento. Asegurate de separar con coma:";
+                                System.out.println(outputChatbox);
+                                data = parser.next();
+                                dataValues = data.split(",");
+                                int edad = Integer.parseInt(dataValues[0]), peso = Integer.parseInt(dataValues[1]), cedula_cliente = Integer.parseInt(dataValues[2]);
+                                String raza = dataValues[3], medicamento = dataValues[4];
+                                Mascota mascota = new Mascota(mascotas.size(), edad, peso, cedula_cliente, nombre, raza, medicamento);
+                                mascotas.set(indexOftargetElement, mascota);
+                                outputChatbox = "Información de la mascota actualizada con satisfacción.";
+                                System.out.println(outputChatbox);
+
+                                break;
+                            case 2:
+                                if (clientes.size() == 0) {
+                                    outputChatbox = "No se han agregado clientes a la lista.";
+                                    System.out.println(outputChatbox);
+                                    generator.reset();
+                                }
+                                for (int i = 0; i < clientes.size(); i++) {
+                                    System.out.println("(" + i + ")" + clientes.get(i));
+                                }
+                                outputChatbox = "Selecciona el indice del elemento que deseas editar:";
+                                System.out.println(outputChatbox);
+                                indexOftargetElement = parser.nextInt();
+                                outputChatbox = "El cliente seleccionado al cual se le cambiará la información es:";
+                                System.out.println(outputChatbox);
+                                System.out.println(clientes.get(indexOftargetElement));
+                                outputChatbox = "Ingresa el nombre del cliente:";
+                                System.out.println(outputChatbox);
+                                nombre = parser.next();
+                                outputChatbox = "Ingresa la teléfono, cédula del cliente, apellidos y dirección. Asegurate de separar con coma:";
+                                System.out.println(outputChatbox);
+                                data = parser.next();
+                                dataValues = data.split(",");
+                                int telefono = Integer.parseInt(dataValues[0]), cc = Integer.parseInt(dataValues[1]);
+                                String apellidos = dataValues[2], direccion = dataValues[3];
+                                Cliente cliente = new Cliente(cc, nombre, apellidos, direccion, telefono);
+                                clientes.set(indexOftargetElement, cliente);
+                                outputChatbox = "Información del cliente actualizada con satisfacción.";
+                                System.out.println(outputChatbox);
+                                
+                                break;
+                            case 3:
+                                if (medicamentos.size() == 0) {
+                                    outputChatbox = "No se han agregado medicamentos a la lista.";
+                                    System.out.println(outputChatbox);
+                                    generator.reset();
+                                }
+                                for (int i = 0; i < medicamentos.size(); i++) {
+                                    System.out.println("(" + i + ")" + medicamentos.get(i));
+                                }
+                                outputChatbox = "Selecciona el indice del elemento que deseas editar:";
+                                System.out.println(outputChatbox);
+                                indexOftargetElement = parser.nextInt();
+                                outputChatbox = "El medicamento seleccionado al cual se le cambiará la información es:";
+                                System.out.println(outputChatbox);
+                                System.out.println(medicamentos.get(indexOftargetElement));
+                                outputChatbox = "Ingresa el nombre del medicamento:";
+                                System.out.println(outputChatbox);
+                                nombre = parser.next();
+                                outputChatbox = "Ingresa la dosis y la descripción. Asegurate de separar con coma:";
+                                System.out.println(outputChatbox);
+                                data = parser.next();
+                                dataValues = data.split(",");
+                                int dosis = Integer.parseInt(dataValues[0]);
+                                String descripcion = dataValues[1];
+                                Medicamento medicamentoClass = new Medicamento(dosis, nombre, descripcion);
+                                medicamentos.set(indexOftargetElement, medicamentoClass);
+                                outputChatbox = "Información del medicamentoactualizada con satisfacción.";
+                                System.out.println(outputChatbox);
+                                
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 4:
+                        outputChatbox = "¿Qué tipo de información deseas eliminar? 1 = Mascota | 2 = Cliente | 3 = Medicamento";
+                        System.out.println(outputChatbox);
+                        prototype = parser.nextInt();
+                        switch (prototype) {
+                            case 1:
+                                if (clientes.size() == 0) {
+                                    outputChatbox = "No se han agregado clientes a la lista.";
+                                    System.out.println(outputChatbox);
+                                    generator.reset();
+                                }
+                                for (int i = 0; i < clientes.size(); i++) {
+                                    System.out.println("(" + i + ")" + clientes.get(i));
+                                }
+                                outputChatbox = "Selecciona el indice del elemento que deseas eliminar:";
+                                System.out.println(outputChatbox);
+                                indexOftargetElement = parser.nextInt();
+                                outputChatbox = "El cliente seleccionado que será eliminado es:";
+                                System.out.println(outputChatbox);
+                                clientes.remove(indexOftargetElement);
+                                outputChatbox = "Cliente con indice (" + indexOftargetElement + ") eliminado satisfactoriamente.";
+                                System.out.println(outputChatbox);
+                                break;
+                            case 2:
+                                if (mascotas.size() == 0) {
+                                    outputChatbox = "No se han agregado mascotas a la lista.";
+                                    System.out.println(outputChatbox);
+                                    generator.reset();
+                                }
+                                for (int i = 0; i < mascotas.size(); i++) {
+                                    System.out.println("(" + i + ")" + mascotas.get(i));
+                                }
+                                outputChatbox = "Selecciona el indice del elemento que deseas eliminar:";
+                                System.out.println(outputChatbox);
+                                indexOftargetElement = parser.nextInt();
+                                outputChatbox = "La mascota seleccionada que será eliminada es:";
+                                System.out.println(outputChatbox);
+                                mascotas.remove(indexOftargetElement);
+                                outputChatbox = "Mascota con indice (" + indexOftargetElement + ") eliminada satisfactoriamente.";
+                                System.out.println(outputChatbox);
+                                break;
+                            case 3:
+                                if (medicamentos.size() == 0) {
+                                    outputChatbox = "No se han agregado medicamentos a la lista.";
+                                    System.out.println(outputChatbox);
+                                    generator.reset();
+                                }
+                                for (int i = 0; i < medicamentos.size(); i++) {
+                                    System.out.println("(" + i + ")" + medicamentos.get(i));
+                                }
+                                outputChatbox = "Selecciona el indice del elemento que deseas eliminar:";
+                                System.out.println(outputChatbox);
+                                indexOftargetElement = parser.nextInt();
+                                outputChatbox = "El medicamento seleccionado que será eliminado es:";
+                                System.out.println(outputChatbox);
+                                medicamentos.remove(indexOftargetElement);
+                                outputChatbox = "Mascota con indice (" + indexOftargetElement + ") eliminada satisfactoriamente.";
+                                System.out.println(outputChatbox);
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
                     default:
                         generator.reset();
                         break;
